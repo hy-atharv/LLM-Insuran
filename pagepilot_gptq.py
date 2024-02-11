a22fc3406b6319890e80eb9b9949d75c948547da
Bhupesh89Kumar <124280654+Bhupesh89Kumar@users.noreply.github.com>
2024-02-11 19:10:57 +0530

    Update pagepilot_gptq.py

--- pagepilot_gptq.py
+++ pagepilot_gptq.py
@@ -1,35 +1,23 @@
 import streamlit as st
 import torch
 from auto_gptq import AutoGPTQForCausalLM
-from langchain import HuggingFacePipeline, PromptTemplate
-from langchain.chains import RetrievalQA
+from langchain import HuggingFacePipeline
 from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from pdf2image import convert_from_path
 from transformers import AutoTokenizer, TextStreamer, pipeline
-
-DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 # Data loading
 pdf_directory = "pdfs"
 loader = PyPDFDirectoryLoader(pdf_directory)
 docs = loader.load()
-embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large", model_kwargs={"device": DEVICE})
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=64)
-texts = text_splitter.split_documents(docs)
+embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
 
 # Model loading
 model_name_or_path = "TheBloke/Llama-2-13B-chat-GPTQ"
 model_basename = "model"
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
-model = AutoGPTQForCausalLM.from_pretrained(model_name_or_path, 
-                                           model_basename=model_basename, 
-                                           use_safetensors=True,
-                                           trust_remote_code=True, 
-                                           inject_fused_attention=False, 
-                                           device=DEVICE,
-                                           quantize_config=None)  # You may need to adjust the quantization config
+model = AutoGPTQForCausalLM.from_pretrained(model_name_or_path, model_basename=model_basename)
 
 # Pipeline setup
 streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
