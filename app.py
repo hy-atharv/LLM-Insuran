2187d6c34055fede737b2936970c5cea563c95fa
Atharv Kumar Tiwari <131706297+hy-atharv@users.noreply.github.com>
2024-02-19 20:26:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ if file is not None:
     content = ""
 
     for page in range(len(pdf_reader.pages)):
-        content += pdf_reader.getPage(page).extractText()
+        content += pdf_reader.pages[page].extractText()
 
     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large", model_kwargs={"device": DEVICE})
 
